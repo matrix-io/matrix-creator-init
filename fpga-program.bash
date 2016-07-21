d59669a954a3eba1d98f13a3dff431be222c57c2
Nelson Castillo <nelson.castillo@admobilize.com>
2016-07-21 08:32:43 -0800

    Reset JTAG before programming the FPGA.

--- fpga-program.bash
+++ fpga-program.bash
@@ -3,7 +3,13 @@
 cd /usr/share/admobilize/matrix-creator
 
 function try_program() {
-    xc3sprog -c matrix_pi blob/system.bit -p 1
+  echo 18 > /sys/class/gpio/export
+  echo out > /sys/class/gpio/gpio18/direction
+  echo 1 > /sys/class/gpio/gpio18/value
+  echo 0 > /sys/class/gpio/gpio18/value
+  echo 1 > /sys/class/gpio/gpio18/value
+  sleep 0.1
+  xc3sprog -c matrix_pi blob/system.bit -p 1
 }
 
 
