56db04b89b247e17fab2827ed835ea6d2619e03a
Kevin Patino <kdpatinos@unal.edu.co>
2017-01-12 14:53:40 +0000

    Fixing radio init script

--- radio-init.bash
+++ radio-init.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo 5 > /sys/class/gpio/export 
-echo out > /sys/class/gpio/gpio5/direction
+for i in 5 13 21
+do
+  if [ ! -d /sys/class/gpio/gpio$i ]
+  then
+    echo $i > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio${i}/direction
+  fi
+done
+
 echo 1 > /sys/class/gpio/gpio5/value
-
-echo 13 > /sys/class/gpio/export 
-echo out > /sys/class/gpio/gpio13/direction
 echo 0 > /sys/class/gpio/gpio13/value
-
-echo 21 > /sys/class/gpio/export 
-echo out > /sys/class/gpio/gpio21/direction
 echo 0 > /sys/class/gpio/gpio21/value
