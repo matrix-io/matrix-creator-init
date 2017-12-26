84d3b408712aa929df4d63a7a7c09e16b1ebf38a
Kevin Patino <kevindpatino@gmail.com>
2017-12-26 03:25:43 +0000

    First Voice set up

---
+++ matrix-init.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#cd /usr/share/admobilize/matrix-creator
+
+function detect_device(){
+  MATRIX_DEVICE=$(./fpga_info | grep IDENTIFY | cut -f 3 -d ' ')
+}
+
+./fpga-program.bash
+detect_device
+
+case "${MATRIX_DEVICE}" in
+  "05c344e8")  
+     echo "*** MATRIX Creator initial process has been launched"
+    ./em358-program.bash
+    ./radio-init.bash
+    ./sam3-program.bash
+    ;;
+  "6032bad2")
+    echo "*** MATRIX Voice initial process has been launched"
+    ;;
+esac
+
+exit 0
