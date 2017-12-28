6d6c0500b84b067b314f5db4fa431d494f470471
Kevin Patino <kevindpatino@gmail.com>
2017-12-28 15:56:32 +0000

    Adding sleep time in reset

--- fpga-program.bash
+++ fpga-program.bash
@@ -15,7 +15,8 @@ function reset_creator(){
   echo out > /sys/class/gpio/gpio18/direction
   echo 1 > /sys/class/gpio/gpio18/value
   echo 0 > /sys/class/gpio/gpio18/value
-  echo 1 > /sys/class/gpio/gpio18/value 
+  echo 1 > /sys/class/gpio/gpio18/value
+  sleep 2 
 }
 
 function try_program_creator() {
